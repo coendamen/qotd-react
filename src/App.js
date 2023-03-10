9a0635c6a9f1efd0e75e5db214fc0aadae8a1db4
Coen Damen <cgmmdamen@gmail.com>
2023-03-10 13:37:24 +0100

    Made modulo quotes based on day of month

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+
+    render() {
+        return (
+          <div className="App">
+              <p>
+              <Quote />
+          </p>
+          </div>
+          );
+    }
+}
+
+class Quote extends Component {
+    render() {
+        const dayOfMonth = new Date().getDate();
+        const i = dayOfMonth % this.props.quotes.length;
+        return "The Quote of the Day is " + this.props.quotes[i];
+    }
+}
+
+Quote.defaultProps = {
+    quotes: ["\"The Quote of the Day\"", "\"Carpe Diem\"", "\"Don't take life so seriously, you won't survive it anyway\""]
+};
+
+export default App;
